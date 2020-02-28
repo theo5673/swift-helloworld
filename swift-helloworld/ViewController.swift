3c3545ba529c8318afbb916875b1b8ed9d270cf6
Theodore Leonard <theodore.g.leonard@gmail.com>
2020-02-28 00:00:00 -0500

    Initial Commit

---
+++ swift-helloworld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift-helloworld
+//
+//  Created by Thedoore Gerald Leonard on 2/27/20.
+//  Copyright © 2020 Thedoore Gerald Leonard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
